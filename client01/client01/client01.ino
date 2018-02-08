f4acc1f40997ea4d915179208daed39957e18337
EkberAziz <ekbr.aziz@gmail.com>
2018-02-08 12:07:33 +0300

    NodeMCU test codes
    simple NodeMCU/ESP8266 test codes

---
+++ client01/client01/client01.ino
@@ -0,0 +1,87 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+const char* ssid     = "ARDIC_GUEST";      // SSID
+const char* password = "w1Cm2ardC";      // Password
+const char* host = "192,168,2,10";
+const uint16_t port = 9877;
+
+void setup() {
+    Serial.begin(9600);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+    WiFi.mode(WIFI_STA);
+    WiFiMulti.addAP(ssid, password);
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Wait for WiFi... ");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    delay(500);
+}
+
+
+void loop() {
+    
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+
+    if (!client.connect(host, port)) {
+        Serial.println("connection failed");
+        Serial.println("wait 5 sec...");
+        delay(5000);
+        return;
+    }
+
+    // This will send the request to the server
+    client.println("Send this data to server");
+
+    //read back one line from server
+    String line = client.readStringUntil('\r');
+    Serial.println(line);
+
+    Serial.println("closing connection");
+    client.stop();
+    
+    Serial.println("wait 5 sec...");
+    delay(5000);
+/*
+    Serial.println("[connected]");
+
+    Serial.println("[Sending a request]");
+    client.print(String("GET /") + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n" +
+                 "\r\n"
+                );
+
+    Serial.println("[Response:]");
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+      }
+    }
+    client.stop();
+    Serial.println("\n[Disconnected]");
+  */
+  delay(5000);
+}
